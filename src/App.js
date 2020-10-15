0651b15dfbcbf157de2a910264a193cd658e74e3
ejiro-ghene <stephanieukiwe@gmail.com>
2020-10-15 06:58:14 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import navbar from './components/landingPage/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
